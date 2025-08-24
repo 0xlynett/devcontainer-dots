737bb9b10f5153fca6ebf6cea77a4a53980e9eff
Lyn <0xlynett@proton.me>
2025-08-24 22:36:55 +0100

    fix folder order

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -sS https://starship.rs/install.sh | sh
+curl -sS https://starship.rs/install.sh | sh --yes
 
 git config --global user.name $GIT_USERNAME
 git config --global user.email $GIT_EMAIL
@@ -8,7 +8,7 @@ git config --global gpg.format ssh
 git config --global user.signingkey ~/.ssh/key
 git config --global commit.gpgsign true
 
-echo $GIT_SIGNING_KEY > ~/.ssh/key
-
 mkdir ~/.ssh
+
+echo $GIT_SIGNING_KEY > ~/.ssh/key
 cp ./ssh-config.txt ~/.ssh/config
