92dba918bde283e9cda521c8c5ab71740e9c0fcf
lynett.eth <0xlynett@proton.me>
2025-10-02 21:04:07 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,10 +3,14 @@
 git config --global user.name $GIT_USERNAME
 git config --global user.email $GIT_EMAIL
 git config --global gpg.format ssh
-git config --global user.signingkey ~/.ssh/key
+git config --global user.signingkey ~/.ssh/key.pub
 git config --global commit.gpgsign true
 
-mkdir ~/.ssh
-
-echo $GIT_SIGNING_KEY > ~/.ssh/key
-cp ./ssh-config.txt ~/.ssh/config
+if [ -n "$GIT_SIGNING_KEY" ] && [ -n "$GIT_SIGNING_KEY_PUB" ]; then
+  mkdir -p ~/.ssh
+  echo "$GIT_SIGNING_KEY" > ~/.ssh/key
+  echo "$GIT_SIGNING_KEY_PUB" > ~/.ssh/key.pub
+  chmod 600 ~/.ssh/key
+  chmod 600 ~/.ssh/key.pub
+  cp ./ssh-config.txt ~/.ssh/config
+fi
